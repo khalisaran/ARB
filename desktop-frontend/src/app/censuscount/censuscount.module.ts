5e505cb71b30f94c774b65de4c4e220f1035747c
DHINAKAR-P <dhinakarintech@gmail.com>
2017-08-29 16:30:27 +0530

    List for census count grid added

--- desktop-frontend/src/app/censuscount/censuscount.module.ts
+++ desktop-frontend/src/app/censuscount/censuscount.module.ts
@@ -3,19 +3,24 @@ import { RouterModule } from '@angular/router';
 import { Ng2Bs3ModalModule } from 'ng2-bs3-modal/ng2-bs3-modal';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CensusCount_screenengComponent } from './censuscount_screeneng.component';
+import { List_CensusCount_screenengComponent } from './list_censuscount_screeneng.component';
 import { MdInputModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
+import { AgGridModule } from 'ag-grid-angular/main';
 import { CensusCountService } from './censuscount.service';
 
 @NgModule({
     declarations: [
-        CensusCount_screenengComponent
+        CensusCount_screenengComponent,
+        List_CensusCount_screenengComponent
     ],
     imports: [
         MdInputModule,
         FormsModule,
+        AgGridModule.withComponents([]),
         RouterModule.forChild([
-            { path: 'CensusCount_screeneng', component: CensusCount_screenengComponent}
+            { path: 'CensusCount_screeneng', component: CensusCount_screenengComponent},
+            { path: 'List_CensusCount_screeneng', component: List_CensusCount_screenengComponent}
         ]),
         Ng2Bs3ModalModule,
         BrowserAnimationsModule
---
+++ desktop-frontend/src/app/censuscount/list_censuscount_screeneng.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { CensusCountService } from './censuscount.service';
+import { GridOptions } from 'ag-grid';
+import { ICensusCount } from './censuscount';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'list_censuscount_screeneng.component.html'
+})
+export class List_CensusCount_screenengComponent implements OnInit {
+  private Table_854GridOptions: GridOptions;
+
+  private censuscount: ICensusCount = {
+  	id: 0,
+  	name: '',
+  	description: ''}
+  ;
+  @ViewChild('modalSFU')
+   mymodalSFU: ModalComponent;
+array_censuscount : any[];
+  constructor(private router: Router, public toastr: ToastsManager, vcr: ViewContainerRef, private censuscountservice: CensusCountService) { 
+    this.toastr.setRootViewContainerRef(vcr);
+    this.Table_854GridOptions = {
+    	columnDefs: [
+    		{
+    			headerName: "Name",
+    			field: "name",
+    		},
+    		{
+    			headerName: "Description",
+    			field: "description",
+    		}
+    	],
+    	rowSelection: 'single',
+    };
+
+  }
+
+  ngOnInit() {
+  /*  if(!this.censuscount.id)
+    	this.mymodalSFU.open();*/
+
+  }
+
+
+  get_all_CensusCount(){
+      this.censuscountservice.get_all_CensusCount()
+          .subscribe(data => {
+            console.log("data", data);
+           // this.toastr.success('Success!');
+           this.array_censuscount = data;
+          },
+          error => {
+            this.toastr.error('Check the browser console to see more info.','Error!');
+          });
+  }
+  search_for_update_CensusCount(){
+    //  this.mymodalSFU.close();
+      this.censuscountservice.search_for_update_CensusCount(this.censuscount.id)
+          .subscribe(data => {
+            console.log("data", data);
+            this.toastr.success('Success!');
+            this.censuscount = data
+          },
+          error => {
+            this.toastr.error('Check the browser console to see more info.','Error!');
+          });
+  }
+
+}
