fad8a34d57bee90de4384a2d145d004572d5ff7f
DHINAKAR-P <dhinakarintech@gmail.com>
2017-08-29 16:34:07 +0530

    pwc ip sync up

--- desktop-frontend/src/app/shared/shared.service.ts
+++ desktop-frontend/src/app/shared/shared.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SharedService {
-    public baseUrl: string = "http://localhost:8080";
+    public baseUrl: string = "http://10.243.76.68:8080";
     public browser_language: string;
 }
